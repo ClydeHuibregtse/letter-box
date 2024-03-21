4f45227854e404613c607a96d3d85bc3bfded469
Clyde Huibregtse <clyde@huibregtse.com>
2024-03-21 17:33:15 -0400

    Benchmarks of various word utils

--- server/benches/words.rs
+++ server/benches/words.rs
@@ -1,27 +1,59 @@
 use divan::{black_box, counter::BytesCount, AllocProfiler, Bencher};
 use letter_boxed::solver::graph::Graph;
 use letter_boxed::solver::lexicon::{Lexicon, LEXICON_PATH};
-use letter_boxed::solver::words::{can_make_word, random_string};
+use letter_boxed::solver::words::{
+    can_make_word, random_english_string, random_string, WordTrajectories, WordTrajectory,
+};
 
 fn main() {
     // Run registered benchmarks.
     divan::main();
 }
 
-#[divan::bench(consts = [4, 8, 16, 32])]
-fn bench_can_make_word<const SIZES: usize>(bencher: Bencher) {
+/// Benchmark the increment of WordTrajectory
+/// (does this allocate unnecessarily? Should we mutate instead?)
+#[divan::bench()]
+fn bench_word_trajectories(bencher: Bencher) {
     bencher
-        .with_inputs(|| {
-            // Generate a random game board and random word
-            (random_string(SIZES), random_string(SIZES))
-        })
-        .bench_local_refs(|args| {
-            let mut trajectories = can_make_word(args.0.as_str(), &args.1);
-            while let Some(next) = trajectories.next() {}
+        .with_inputs(|| WordTrajectory::new())
+        .bench_local_values(|traj| {
+            traj.add_index(0);
         });
 }
 
-#[divan::bench(consts = [4, 8, 16, 32, 64], sample_count=10)]
+/// Benchmark the end to end computation of valid word trajectories
+#[divan::bench(consts = [4, 8, 16, 32, 40, 45])]
+fn bench_can_make_word<const SIZES: usize>(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            // Generate a random word and random game board
+            (random_string(SIZES / 4), random_string(SIZES))
+        })
+        .bench_local_refs(|args| {
+            let mut trajectories = can_make_word(args.0.as_str(), &args.1);
+            trajectories.next();
+        });
+}
+
+/// Benchmark the end to end computation of valid word trajectories
+/// using random strings with English letter distribution
+#[divan::bench(consts = [4, 8, 16, 32, 40, 45])]
+fn bench_can_make_word_english<const SIZES: usize>(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            // Generate a random word and random game board
+            (
+                random_english_string(SIZES / 4),
+                random_english_string(SIZES),
+            )
+        })
+        .bench_local_refs(|args| {
+            let mut trajectories = can_make_word(args.0.as_str(), &args.1);
+            trajectories.next();
+        });
+}
+
+#[divan::bench(consts = [4, 8, 12], sample_count=10)]
 fn bench_solve<const SIZES: usize>(bencher: Bencher) {
     let lexicon = Lexicon::new(LEXICON_PATH).unwrap();
     bencher
@@ -30,6 +62,7 @@ fn bench_solve<const SIZES: usize>(bencher: Bencher) {
             random_string(SIZES)
         })
         .bench_local_refs(|letters| {
+            // println!("{:?}", letters);
             let mut g = Graph::from_letters(letters);
             let words = g.solve(letters, &lexicon);
         });
